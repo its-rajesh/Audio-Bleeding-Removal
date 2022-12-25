c817da2a4a56d4bd7fa6a725779a16f63a81b8fc
Rajesh <rajesh.krsk@gmail.com>
2022-12-25 15:43:11 +0530

    SDR, changes and epochs updated

--- DeepConv.py
+++ DeepConv.py
@@ -13,9 +13,15 @@ from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Conv2
 Flatten, Dropout, BatchNormalization, Reshape
 from keras.models import Model
 
-import numpy as np
 from matplotlib import pyplot as plt
+
+import numpy as np
 import os
+import librosa as lb
+import cmath
+import soundfile as sf
+
+import museval
 
 ##############################################################################################################
 
@@ -38,7 +44,7 @@ print("Getting Magnitude & Phase")
 xtrain = np.array([np.abs(i) for i in xtrain])
 xtest, xtest_phase = np.array([np.abs(i) for i in xtest]), np.array([np.angle(i) for i in xtest])
 ytrain = np.array([np.abs(i) for i in ytrain])
-ytest = np.array([np.abs(i) for i in ytest])
+ytest, ytest_phase = np.array([np.abs(i) for i in ytest]), np.array([np.angle(i) for i in ytest])
 print("Model Initializing....")
 
 ##############################################################################################################
@@ -46,7 +52,7 @@ print("Model Initializing....")
 # ### Architecture
 
 batch_size = 16
-epochs = 50
+epochs = 100
 inp = Input(shape=(1025, 5168, 1))
 
 
@@ -96,31 +102,19 @@ print("Model Initiated")
 ##############################################################################################################
 
 # Autoencoder
-deepconv = Model(inp, x)
+deepconv = Model(inp, decoded)
 deepconv.compile(optimizer="adam", loss="mse")
 #deepconv.summary()
 
 ##############################################################################################################
 
 print("Training...")
-deepconv.fit(
+dc = deepconv.fit(
     x=xtrain,
     y=ytrain,
     epochs=epochs,
     batch_size=batch_size,
     shuffle=True, verbose=1)
-
-##############################################################################################################
-
-print("Evaluation...")
-loss, acc = deepconv.evaluate(xtest,  ytest, verbose=1)
-print("Test Loss: {}\n Test Accuracy: {}".format(loss, acc))
-
-##############################################################################################################
-
-print("Prediction...")
-predictions = deepconv.predict(xtest, verbose=1)
-print("Completed (1/2)")
 
 ##############################################################################################################
 
@@ -130,7 +124,22 @@ print('FINAL MODEL SAVED SUCCESSFULLY IN ({})'.format(savemodelpath))
 
 ##############################################################################################################
 
-print("Creating unbleed samples with trained model")
+print("Evaluation...")
+loss = deepconv.evaluate(xtest,  ytest, verbose=1)
+print("Test Loss: {}".format(loss))
+
+##############################################################################################################
+
+print("Prediction...")
+predictions = deepconv.predict(xtest, verbose=1)
+np.save('./predictions.npy',predictions)
+print('Predictions Saved')
+print("Completed (1/3)")
+
+predictions = np.squeeze(predictions, axis=3)
+
+##############################################################################################################
+
 
 def convert_complex(mag, phase):
     m, n = mag.shape
@@ -154,6 +163,33 @@ def calc_istft(mag, phase):
 
 print("Working on ISTFT.....")
 reconstructed = calc_istft(predictions, xtest_phase)
+true = calc_istft(ytest, ytest_phase)
+
+##############################################################################################################
+
+print("Calculating Evaluation Metrics")
+sdr, isr, sir, sar = museval.evaluate(true, reconstructed, win=44100, hop=44100, mode='v4', padding=True)
+
+
+def get_metrics(y):
+    avg_y = []
+    for i in range(len(y)):
+        x = y[~np.isnan(y)]
+        avg = sum(x)/len(x)
+        avg_y.append(avg)
+    return avg_y
+    
+    
+avg_sdr = get_metrics(sdr)
+avg_isr = get_metrics(isr) #Source to Spatial Distortion Image
+avg_sir = get_metrics(sir)
+avg_sar = get_metrics(sar)
+
+print('Overall SDR: ', sum(avg_sdr)/len(avg_sdr))
+print('Overall ISR: ', sum(avg_isr)/len(avg_isr))
+print('Overall SIR: ', sum(avg_sir)/len(avg_sir))
+print('Overall SAR: ', sum(avg_sar)/len(avg_sar))
+print("Completed (2/3)")
 
 ##############################################################################################################
 
@@ -163,12 +199,18 @@ os.makedirs(out_path, exist_ok=True)
 k = 1
 n = len(reconstructed)
 for audio in reconstructed:
-    sf.write(out_path + '/reconstructed{}.wav'.format(k), audio, 8000)
+    sf.write(out_path + '/reconstructed{}.wav'.format(k), audio, 22050)
+    print("{} files written out of {}".format(k, n))
     k += 1
-    if k % 10 == 0:
-        print("{} files written out of {}".format(k, n))
 
-print("Completed (2/2)")
+print("Completed (3/3)")
+
+##############################################################################################################
+
+plt.plot(dc.history['loss'])
+plt.legend(['training'], loc = 'upper left')
+plt.savefig('loss.png')
+
 print("END")
 
 ##############################################################################################################
