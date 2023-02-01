74cf00f088f3966a50e0f1e97aebf278a0c76149
Rajesh <rajesh.krsk@gmail.com>
2023-02-01 11:43:13 +0530

    Add files via upload

--- DI_Bleed/3G_ComplexInput/train.py
+++ DI_Bleed/3G_ComplexInput/train.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# Convolutional - Reccurent Complex Autoencoder Architecture for bleeding removal (VOCAL)
+#  Complex Convolutional Autoencoder Architecture (CCAE) for vocal bleeding removal
 
 # Author: Rajesh R
 # Git: its-rajesh
 # Date: 23rd December 2022
 
-from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Conv2DTranspose, \
-Flatten, Dropout, BatchNormalization, Reshape
-from keras.models import Model
+from tensorflow import keras
+from tensorflow.keras.layers import Input, Dense, UpSampling3D, Conv2DTranspose,\
+Flatten, MaxPooling3D, BatchNormalization, Reshape, Conv3D, Conv3DTranspose, Conv1DTranspose
+from tensorflow.keras.models import Model
 
 import numpy as np
 from matplotlib import pyplot as plt
 import os
-
-
 
 
 path = "/home/anchal/Desktop/rajesh/complex_di/"
@@ -29,81 +28,53 @@ print("Dataset loaded successfully")
 print(xtrain.shape, ytrain.shape, xtest.shape, ytest.shape)
 
 
-print("Model Initiated..")
-
-##############################################################################################################
-#                                                ARCHITECTURE                                                #
-##############################################################################################################
 
 batch_size = 16
 epochs = 100
-inp = Input(shape=(3075, 3, 1))
+inp = Input(shape=(2, 3075, 3, 1))
 
-################################################## ENCODER ##################################################
 
-# Conv1 Layer #
-x = Conv2D(filters = 32, kernel_size = (2, 2), activation='relu')(inp)
+
+x = Conv3D(1, (2, 1, 3), activation="relu")(inp)
+x = BatchNormalization()(x)
+x = MaxPooling3D(pool_size = (1, 3, 1))(x)
+
+x = Conv3D(512, (1, 15, 1), activation="relu")(x)
+x = BatchNormalization()(x)
+x = MaxPooling3D(pool_size = (1, 3, 1))(x)
+
+x = Conv3D(1024, (1, 21, 1), activation="relu")(x)
 x = BatchNormalization()(x)
 
-# Conv2 Layer #
-x = Conv2D(filters = 64, kernel_size = (2, 2), activation='relu')(x)
-encode = BatchNormalization()(x)
-
-################################################## MIDDLE ###################################################
-
-x = Flatten()(encode)
-
-middle = Dense(100, activation='relu')(x)
-
-dense = Dense(encode.shape[1] * encode.shape[2] * encode.shape[3], activation='relu')(middle)
-
-reshape = Reshape((encode.shape[1], encode.shape[2], encode.shape[3]))(dense)
-
-################################################## DECODER ##################################################
-
-# TConv2 Layer #
-x = Conv2DTranspose(filters = 32, kernel_size = (2, 2), activation='relu')(reshape)
+x = Conv3DTranspose(512, (1, 21, 1), activation="relu")(x)
 x = BatchNormalization()(x)
+x = UpSampling3D(size=(1, 3, 1))(x)
 
-# TConv1 Layer #
-x = Conv2DTranspose(filters = 1, kernel_size = (2, 2), activation='relu')(x)
-decode = BatchNormalization()(x)
+x = Conv3DTranspose(1, (2, 15, 3), activation="relu")(x)
+x = BatchNormalization()(x)
+out = UpSampling3D(size=(1, 3, 1))(x)
 
-##############################################################################################################
-#                                                Autoencoder                                                 #
-##############################################################################################################
 
-di3_dca = Model(inp, decode)
-di3_dca.compile(optimizer="adam", loss="mse")
+complexnn = Model(inp, out)
+complexnn.compile(optimizer="adam", loss="mse")
 
-##############################################################################################################
-#                                                TRAINING                                                    #
-##############################################################################################################
-print("Training...")
-cae = di3_dca.fit(
+complexnn_train = complexnn.fit(
     x=xtrain,
     y=ytrain,
     epochs=epochs,
     batch_size=batch_size,
     shuffle=True, verbose=1)
-
-##############################################################################################################
-
-savemodelpath = './bleed_CAE.h5'
-di3_dca.save(savemodelpath)
+    
+    
+savemodelpath = './complexnn.h5'
+complexnn.save(savemodelpath)
 print('FINAL MODEL SAVED SUCCESSFULLY IN ({})'.format(savemodelpath))
 
-##############################################################################################################
-
-print("Evaluation...")
-loss = di3_dca.evaluate(xtest,  ytest, verbose=1)
+print('Testing.....')
+loss = complexnn.evaluate(xtest,  ytest, verbose=1)
 print("Test Loss: {}".format(loss))
 
-##############################################################################################################
-
-print("Prediction...")
-predictions = di3_dca.predict(xtest, verbose=1)
-np.save('./predictions.npy', predictions)
-print('Predictions Saved')
-
-##############################################################################################################
+plt.plot(complexnn_train.history['loss'])
+plt.legend(['training'], loc = 'upper left')
+plt.savefig('loss.png')
+print('END')
