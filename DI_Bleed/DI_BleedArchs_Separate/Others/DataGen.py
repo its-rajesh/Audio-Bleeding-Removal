3e98fcacb2f43949976863f26e822e4ab7b06b22
Rajesh <rajesh.krsk@gmail.com>
2023-02-24 18:29:09 +0530

    Add files via upload

---
+++ DI_Bleed/DI_BleedArchs_Separate/Others/DataGen.py
@@ -0,0 +1,46 @@
+####################################################################################################
+#                         DRIVER TO GENERATE INPUT DATA (OTHERS: MUSDB18HQ)                         #
+####################################################################################################
+# Author: Rajesh R (Git: its-rajesh)
+# Date: 14/Januray/2023
+
+
+import os
+import librosa as lb
+import numpy as np
+from tqdm import tqdm
+
+import DynamicInput as dp
+
+path = "/home/anchal/Desktop/rajesh/Datasets/musdb18hq_bleeded/test/"
+org_path = "/home/anchal/Desktop/rajesh/Datasets/musdb18hq/test/"
+
+
+flag = False
+print("Processing Inputs Please Wait...(May take several minutes)")
+for i in tqdm(sorted(os.listdir(path))): #INPUT NO OF FILES AT ONCE
+    
+
+    bleed, fs = lb.load(path+i+'/other.wav', sr=22050)
+    true, fs = lb.load(org_path+i+'/other.wav', sr=22050)
+    
+    stft_bleed = lb.stft(bleed, hop_length = 2048)
+    stft_true = lb.stft(true, hop_length = 2048)
+    
+    encode_bleed = dp.DynamicInput.encode(dp.DynamicInput, stft_bleed, 3, 1)
+    encode_true = dp.DynamicInput.encode(dp.DynamicInput, stft_true, 3, 1)
+    
+    if not flag:
+        xtrain = encode_bleed
+        ytrain = encode_true
+        flag = True
+    else:
+        xtrain = np.concatenate((xtrain, encode_bleed), axis=0)
+        ytrain = np.concatenate((ytrain, encode_true), axis=0)
+
+
+print("Writting numpy files...")
+np.save('/home/anchal/Desktop/rajesh/DI_Bleed_Archs/Separate/Others/xtest_other.npy', xtrain)
+np.save('/home/anchal/Desktop/rajesh/DI_Bleed_Archs/Separate/Others/ytest_other.npy', ytrain)
+
+print(xtrain.shape, ytrain.shape)
