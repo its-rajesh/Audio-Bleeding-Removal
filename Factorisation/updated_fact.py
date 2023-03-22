007f25ab84dbf80a8cbb677a061e216c3d4def29
Rajesh <rajesh.krsk@gmail.com>
2023-03-22 19:07:04 +0530

    code2

--- Factorisation/updated_fact.py
+++ Factorisation/updated_fact.py
@@ -4,7 +4,7 @@ import librosa as lb
 import soundfile as sf
 from matplotlib import pyplot as plt
 import os
-
+from tqdm import tqdm
 
 def objective(params, X, m, gamma1, gamma2):
     n, l = X.shape
@@ -29,7 +29,7 @@ def minimize_function(X, m, gamma1, gamma2):
 
     # initialize A as identity matrix and S as X
     n, l = X.shape
-    print(n, l)
+    #print(n, l)
     A_init = np.eye(n)
     S_init = X.copy()
 
@@ -43,7 +43,7 @@ def minimize_function(X, m, gamma1, gamma2):
     res = minimize(objective, params, args=(X, m, gamma1, gamma2), method='L-BFGS-B', bounds=bounds)
 
     # extract the optimized A and S
-    print(n, l)
+    #print(n, l)
     A_opt = res.x[:n**2].reshape((n, n))
     S_opt = res.x[n**2:].reshape((n, l))
     
@@ -51,13 +51,13 @@ def minimize_function(X, m, gamma1, gamma2):
 
 
 print('Reading Input Audio...')
-bleed_path = '/DATA1/rajeshr/project/Datasets/musdb18hq_bleeded/train/Music Delta - Hendrix/'
+bleed_path = '/home/rajesh/Desktop/Datasets/musdb18hq_bleeded/train/Music Delta - Hendrix/'
 bvocals, fs = lb.load(bleed_path+'vocals.wav')
 bbass, fs = lb.load(bleed_path+'bass.wav')
 bdrums, fs = lb.load(bleed_path+'drums.wav')
 bother, fs = lb.load(bleed_path+'other.wav')
 
-clean_path = '/DATA1/rajeshr/project/Datasets/musdb18hq/train/Music Delta - Hendrix/'
+clean_path = '/home/rajesh/Desktop/Datasets/musdb18hq/train/Music Delta - Hendrix/'
 vocals, fs = lb.load(clean_path+'vocals.wav')
 bass, fs = lb.load(clean_path+'bass.wav')
 drums, fs = lb.load(clean_path+'drums.wav')
@@ -88,20 +88,43 @@ m = mixture[:n_]
 X = np.array([bvocals, bbass, bdrums, bother])
 S = np.array([vocals, bass, drums, other])
 
+X = X[:, :88200]
+S = S[:, :88200]
+m = m[:88200]
+
 print(X.shape, S.shape, m.shape)
 
-print('Optimizing...')
+    
+blocks = 25
+print('Dividing {} blocks...'.format(blocks))
 
-gamma1 = 0
+n_, l_ = X.shape
+XChunk, SChunk, mChunk = [], [], []
+for i in range(0, l_, blocks):
+    XChunk.append(X[:, i:i+blocks])
+    SChunk.append(X[:, i:i+blocks])
+    mChunk.append(X[:, i:i+blocks])
+
+run = len(mChunk)
+
+gamma1 = -1
 gamma2 = 1
-A_opt, S_opt = minimize_function(X, m, gamma1, gamma2)
-l1 = np.linalg.norm(X - np.dot(A_opt, S_opt))
-l2 = np.linalg.norm(m - (S_opt[0]+S_opt[1]+S_opt[2]))
-print(l1, l2, l1+l2)
 
-out = '/home/s21005/research/Factorization/CODE2/'
-sf.write(out+'pred_vocal.wav', S_opt[0], fs)
-sf.write(out+'pred_bass.wav', S_opt[1], fs)
-sf.write(out+'pred_drums.wav', S_opt[2], fs)
-sf.write(out+'pred_other.wav', S_opt[3], fs)
+S_arr = []
+print('Optimizing...')
+for i in range(run):
+    A_opt, S_opt = minimize_function(XChunk[i], mChunk[i], gamma1, gamma2)
+    l1 = np.linalg.norm(XChunk[i] - np.dot(A_opt, S_opt))
+    l2 = np.linalg.norm(mChunk[i] - (S_opt[0]+S_opt[1]+S_opt[2]))
+    print("[BLOCK: {}/{}] ||X-AS||^2: {}, ||m-t||^2: {}, LOSS: {}".format(i+1, run, np.round(l1, 5), np.round(l2, 5), np.round(l1+l2, 5)))
+    S_arr.append(S_opt)
+
+S_pred = np.hstack(S_arr)
+
+print('Writing Files...')
+out = '/home/rajesh/Desktop/'
+sf.write(out+'pred_vocal.wav', S_pred[0], fs)
+sf.write(out+'pred_bass.wav', S_pred[1], fs)
+sf.write(out+'pred_drums.wav', S_pred[2], fs)
+sf.write(out+'pred_other.wav', S_pred[3], fs)
 print('DONE!!!')
