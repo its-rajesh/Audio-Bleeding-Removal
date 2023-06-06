ead265997ad5f81205d01e98b5ccb384bc36f20e
Rajesh <rajesh.krsk@gmail.com>
2023-06-06 16:45:40 +0530

    gcn

---
+++ ChebGCN/iwaveunnet_ge.py
@@ -0,0 +1,213 @@
+from spektral.layers import ChebConv
+from tensorflow.keras.layers import LeakyReLU, Conv2D, concatenate, Conv2DTranspose, MaxPooling2D, BatchNormalization
+from tensorflow.keras.layers import Dropout, Input, Dense, Reshape
+from tensorflow.keras.models import Model, load_model, save_model
+import tensorflow as tf
+import numpy as np
+#from matplotlib import pyplot as plt
+from tqdm import tqdm
+import museval
+import pandas as pd
+
+from tensorflow.keras.layers import Layer
+
+
+
+def encoder(input_shape):
+
+    #Downsampling block 1
+    x = Conv2D(16, (4, 4), padding = "same", dilation_rate=1)(input_shape)
+    x = Conv2D(16, (4, 4), padding = "same", dilation_rate=1)(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.3)(x)
+    x = MaxPooling2D((1, 2))(x)
+    ds1 = Dropout(0.1)(x)
+
+    #Downsampling block 2
+    x = Conv2D(32, (4, 4), padding = "same", dilation_rate=2)(ds1)
+    x = Conv2D(32, (4, 4), padding = "same", dilation_rate=2)(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.3)(x)
+    x = MaxPooling2D((1, 2))(x)
+    ds2 = Dropout(0.1)(x)
+
+    #Downsampling block 3
+    x = Conv2D(64, (4, 4), padding = "same", dilation_rate=4)(ds2)
+    x = Conv2D(64, (4, 4), padding = "same", dilation_rate=4)(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.3)(x)
+    x = MaxPooling2D((1, 2))(x)
+    ds3 = Dropout(0.1)(x)
+
+    #Downsampling block 4
+    x = Conv2D(128, (4, 4), padding = "same", dilation_rate=16)(ds3)
+    x = Conv2D(128, (4, 4), padding = "same", dilation_rate=16)(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.3)(x)
+    x = MaxPooling2D((1, 2))(x)
+    ds4 = Dropout(0.1)(x)
+
+    return ds4, ds3, ds2, ds1
+
+class GraphCNN(Layer):
+    def __init__(self, input_dim, **kwargs):
+        super(GraphCNN, self).__init__(**kwargs)
+        self.input_dim = input_dim
+        self.gcn1 = ChebConv(64, K=1, activation=None)
+        self.gcn2 = ChebConv(input_dim, K=1, activation=None)
+        #self.dense = Dense(input_dim, activation='relu')
+    
+    def call(self, inputs):
+        graph_input, adjacency_input = inputs
+        hidden = self.gcn1([graph_input, adjacency_input])
+        hidden = LeakyReLU(alpha=0.3)(hidden)
+        hidden = self.gcn2([hidden, adjacency_input])
+        hidden = LeakyReLU(alpha=0.3)(hidden)
+        #output = self.dense(hidden)
+        return hidden #output
+    
+    def get_config(self):
+        config = super(GraphCNN, self).get_config()
+        config['input_dim'] = self.input_dim
+        return config
+
+
+def bottleneck(encoder_output):
+    # Bottleneck layer
+    x = Conv2D(256, (4, 4), padding = "same")(encoder_output)
+    x = Conv2D(256, (4, 4), padding = "same")(x)
+    x = BatchNormalization()(x)
+    out = LeakyReLU(alpha=0.3)(x)
+    
+    return out
+
+
+def decoder(bottleneck_output, ds4, ds3, ds2, ds1):
+
+    #Upsampling Block 4
+    x = Conv2DTranspose(128, (4, 4), (1, 1), padding = "same")(bottleneck_output)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.3)(x)
+    x = concatenate([x, ds4])
+
+    x = Conv2D(128, (4, 4), padding = "same")(x)
+    x = Conv2D(128, (4, 4), padding = "same")(x)
+    up4 = LeakyReLU(alpha=0.3)(x)
+
+    #Upsampling Block 3
+    x = Conv2DTranspose(64, (4, 4), (1, 2), padding = "same")(up4)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.3)(x)
+    x = concatenate([x, ds3])
+
+    x = Conv2D(64, (4, 4), padding = "same")(x)
+    x = Conv2D(64, (4, 4), padding = "same")(x)
+    up3 = LeakyReLU(alpha=0.3)(x)
+
+    #Upsampling Block 2
+    x = Conv2DTranspose(32, (4, 4), (1, 2), padding = "same")(up3)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.3)(x)
+    x = concatenate([x, ds2])
+
+    x = Conv2D(32, (4, 4), padding = "same")(x)
+    x = Conv2D(32, (4, 4), padding = "same")(x)
+    up2 = LeakyReLU(alpha=0.3)(x)
+
+    #Upsampling Block 1
+    x = Conv2DTranspose(16, (4, 4), (1, 2), padding = "same")(up2)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.3)(x)
+    x = concatenate([x, ds1])
+
+    x = Conv2D(32, (4, 4), padding = "same")(x)
+    x = Conv2D(32, (4, 4), padding = "same")(x)
+    up1 = LeakyReLU(alpha=0.3)(x)
+
+    return up1
+
+
+def iwaveUnet(Xtrain, Ytrain, batch_size, epochs):
+
+    channels = 4
+    samples = 220448
+    input_shape = Input(shape=(channels, samples, 1))
+
+    # ENCODER
+    ds4, ds3, ds2, ds1 = encoder(input_shape)
+
+    # GRAPH CONVOLUIONAL NEURAL NETWORK
+    reshaped = tf.reshape(ds4, shape=(-1, 4, ds4.shape[2]*ds4.shape[3]))
+    num_nodes = 4
+    input_dim = ds4.shape[2]*ds4.shape[3]
+    model = GraphCNN(input_dim)
+
+    A = np.ones((num_nodes, num_nodes)) - np.eye(num_nodes)
+    A = np.stack([A]*Xtrain.shape[0])
+    #A = tf.convert_to_tensor(A, dtype=tf.float32)
+
+    graph_input = Input(shape=(num_nodes, input_dim))
+    adjacency_input = Input(shape=(num_nodes, num_nodes))
+
+    embedding = model([graph_input, adjacency_input])
+    x = model([reshaped, A])
+    reverted_output = Reshape(target_shape=(4, ds4.shape[2], ds4.shape[3]))(x)
+    
+
+    # CONCAT AND REPRESENT OTHER SHAPE
+    con = concatenate([reverted_output, ds4])
+    same_dim = Conv2D(1, (4, 4), padding = "same")(con)
+
+
+    # BOTTLE NECK
+    bottleneck_output = bottleneck(same_dim)
+
+    # DECODER
+    decoder_output = decoder(bottleneck_output, ds4, ds3, ds2, ds1)
+
+    x = Conv2DTranspose(1, (4, 4), (1, 2), padding="same")(decoder_output)
+    output = LeakyReLU(alpha=0.3)(x)
+
+    iwaveunet = Model(input_shape, output, name="iWaveUnet_ge")
+
+    iwaveunet.compile(optimizer='adam', loss='mse')
+
+    history = iwaveunet.fit(Xtrain, Ytrain, batch_size=batch_size, epochs=epochs)
+
+    iwaveunet.save('iwaveunet_ge.h5')
+
+
+    print('Model Saved')
+
+
+
+################################################################
+#                        DRIVER CODE                           #
+################################################################
+
+if __name__ == "__main__":
+
+    dpath = "/home/anchal/Desktop/rajesh/Datasets/extensive_bleed/"
+
+    print('Loading Files...')
+
+    Xtrain = np.load(dpath+'Xtrain.npy')
+    Ytrain = np.load(dpath+'Ytrain.npy')
+    Xtest = np.load(dpath+'Xtest.npy')
+    Ytest = np.load(dpath+'Ytest.npy')
+
+
+    Xtrain = np.vstack([Xtrain, Xtest])
+    Ytrain = np.vstack([Ytrain, Ytest])
+
+    dim = 220448 #Note
+
+    Xtrain = Xtrain[:, :, :dim]
+    Ytrain = Ytrain[:, :, :dim]
+
+    print(Xtrain.shape, Ytrain.shape)
+
+    print('Training....')
+    batch_size = 1
+    epochs = 100
+    iwaveUnet(Xtrain, Ytrain, batch_size, epochs)
---
+++ ChebGCN/test.py
@@ -0,0 +1,209 @@
+import numpy as np
+import tensorflow as tf
+from tqdm import tqdm
+import museval
+import pandas as pd
+
+
+def get_metrics(y):
+    avg_y = []
+    for i in range(len(y)):
+        x = y[~np.isnan(y)]
+        avg = sum(x)/(len(x)+0.0000001)
+        avg_y.append(avg)
+    return avg_y
+
+def compute_sdr(true, reconstructed, fs):
+    t = np.array([true])
+    r = np.array([reconstructed])
+
+    sdr, isr, sir, sar = museval.evaluate(t, r, win=fs, hop=fs)
+        
+    avg_sdr = get_metrics(sdr)
+    avg_isr = get_metrics(isr) #Source to Spatial Distortion Image
+    avg_sir = get_metrics(sir)
+    avg_sar = get_metrics(sar)
+
+    return sum(avg_sdr)/len(avg_sdr), sum(avg_isr)/len(avg_isr), sum(avg_sir)/len(avg_sir), sum(avg_sar)/len(avg_sar)
+
+def calc_sdr(model, path):
+    print('Loading Files...')
+    path2 = "/home/anchal/Desktop/rajesh/TESTING/Test2/"
+    path3 = "/home/anchal/Desktop/rajesh/TESTING/Test1/"
+
+    x1 = np.load(path2+'xevaluate_lh.npy')
+    y1 = np.load(path2+'yevaluate_lh.npy')
+    x2 = np.load(path3+'xevaluate.npy')
+    y2 = np.load(path3+'yevaluate.npy')
+
+
+    Xtest = np.vstack([x1, x2])
+    Ytest = np.vstack([y1, y2])
+
+    dim = 220448
+
+    X_test = Xtest[:, :, :dim]
+    Y_test = Ytest[:, :, :dim]
+
+    print(X_test.shape, Y_test.shape)
+
+    print("Evaluating..")
+
+    
+    predictions = []
+    for i in tqdm(range(0, X_test.shape[0])):
+        predictions.append(model.predict(X_test[i:i+1, :, :]))
+    predictions = np.vstack(predictions)
+    
+    print(predictions.shape)
+
+    fs = 22050
+    Atv_sdr, Atv_isr, Atv_sir, Atv_sar = [], [], [], []
+    Atb_sdr, Atb_isr, Atb_sir, Atb_sar = [], [], [], []
+    Atd_sdr, Atd_isr, Atd_sir, Atd_sar = [], [], [], []
+    Ato_sdr, Ato_isr, Ato_sir, Ato_sar = [], [], [], []
+
+    Av_sdr, Av_isr, Av_sir, Av_sar = [], [], [], []
+    Ab_sdr, Ab_isr, Ab_sir, Ab_sar = [], [], [], []
+    Ad_sdr, Ad_isr, Ad_sir, Ad_sar = [], [], [], []
+    Ao_sdr, Ao_isr, Ao_sir, Ao_sar = [], [], [], []
+
+    Atrue_sdr, Atrue_isr, Atrue_sir, Atrue_sar = [], [], [], []
+    Aobtained_sdr, Aobtained_isr, Aobtained_sir, Aobtained_sar = [], [], [], []
+    L2norm = []
+
+    for i in tqdm(range(X_test.shape[0])):
+
+        S_pred = predictions[i]
+        
+        tv_sdr, tv_isr, tv_sir, tv_sar = compute_sdr(Y_test[i][0], X_test[i][0], fs)
+        tb_sdr, tb_isr, tb_sir, tb_sar = compute_sdr(Y_test[i][1], X_test[i][1], fs)
+        td_sdr, td_isr, td_sir, td_sar = compute_sdr(Y_test[i][2], X_test[i][2], fs)
+        to_sdr, to_isr, to_sir, to_sar = compute_sdr(Y_test[i][3], X_test[i][3], fs)
+
+        true_sdr = (tv_sdr + tb_sdr + td_sdr + to_sdr)/4
+        true_isr = (tv_isr + tb_isr + td_isr + to_isr)/4
+        true_sir = (tv_sir + tb_sir + td_sir + to_sir)/4
+        true_sar = (tv_sar + tb_sar + td_sar + to_sar)/4
+        #print(true_sdr, tv_sdr, tb_sdr, td_sdr, to_sdr)
+
+        v_sdr, v_isr, v_sir, v_sar = compute_sdr(Y_test[i][0], S_pred[0], fs)
+        b_sdr, b_isr, b_sir, b_sar = compute_sdr(Y_test[i][1], S_pred[1], fs)
+        d_sdr, d_isr, d_sir, d_sar = compute_sdr(Y_test[i][2], S_pred[2], fs)
+        o_sdr, o_isr, o_sir, o_sar = compute_sdr(Y_test[i][3], S_pred[3], fs)
+
+        obtained_sdr = (v_sdr + b_sdr + d_sdr + o_sdr)/4
+        obtained_isr = (v_isr + b_isr + d_isr + o_isr)/4
+        obtained_sir = (v_sir + b_sir + d_sir + o_sir)/4
+        obtained_sar = (v_sar + b_sar + d_sar + o_sar)/4
+        #print(obtained_sdr, v_sdr, b_sdr, d_sdr, o_sdr)
+
+        Atv_sdr.append(tv_sdr)
+        Atv_isr.append(tv_isr)
+        Atv_sir.append(tv_sir)
+        Atv_sar.append(tv_sar)
+
+        Atb_sdr.append(tb_sdr)
+        Atb_isr.append(tb_isr)
+        Atb_sir.append(tb_sir)
+        Atb_sar.append(tb_sar)
+
+        Atd_sdr.append(td_sdr)
+        Atd_isr.append(td_isr)
+        Atd_sir.append(td_sir)
+        Atd_sar.append(td_sar)
+
+        Ato_sdr.append(to_sdr)
+        Ato_isr.append(to_isr)
+        Ato_sir.append(to_sir)
+        Ato_sar.append(to_sar)
+
+        Av_sdr.append(v_sdr)
+        Av_isr.append(v_isr)
+        Av_sir.append(v_sir)
+        Av_sar.append(v_sar)
+
+        Ab_sdr.append(b_sdr)
+        Ab_isr.append(b_isr)
+        Ab_sir.append(b_sir)
+        Ab_sar.append(b_sar)
+
+        Ad_sdr.append(d_sdr)
+        Ad_isr.append(d_isr)
+        Ad_sir.append(d_sir)
+        Ad_sar.append(d_sar)
+
+        Ao_sdr.append(o_sdr)
+        Ao_isr.append(o_isr)
+        Ao_sir.append(o_sir)
+        Ao_sar.append(o_sar)
+
+        Atrue_sdr.append(true_sdr)
+        Atrue_isr.append(true_isr)
+        Atrue_sir.append(true_sir)
+        Atrue_sar.append(true_sar)
+
+        Aobtained_sdr.append(obtained_sdr)
+        Aobtained_isr.append(obtained_isr)
+        Aobtained_sir.append(obtained_sir)
+        Aobtained_sar.append(obtained_sar)
+
+
+        
+
+    sdr = pd.DataFrame(
+        {'vocal sdr': Atv_sdr,
+        'bass sdr': Atb_sdr,
+        'drums sdr': Atd_sdr,
+        'other sdr': Ato_sdr,
+        'Overall sdr': Atrue_sdr,
+        'vocal isr': Atv_isr,
+        'bass isr': Atb_isr,
+        'drums isr': Atd_isr,
+        'other isr': Ato_isr,
+        'Overall isr': Atrue_isr,
+        'vocal sir': Atv_sir,
+        'bass sir': Atb_sir,
+        'drums sir': Atd_sir,
+        'other sir': Ato_sir,
+        'Overall sir': Atrue_sir,
+        'vocal sar': Atv_sar,
+        'bass sar': Atb_sar,
+        'drums sar': Atd_sar,
+        'other sar': Ato_sar,
+        'Overall sar': Atrue_sar,
+        'Pred vocal sdr': Av_sdr,
+        'Pred bass sdr': Ab_sdr,
+        'Pred drums sdr': Ad_sdr,
+        'Pred other sdr': Ao_sdr,
+        'Pred Overall sdr': Aobtained_sdr,
+        'Pred vocal isr': Av_isr,
+        'Pred bass isr': Ab_isr,
+        'Pred drums isr': Ad_isr,
+        'Pred other isr': Ao_isr,
+        'Pred Overall isr': Aobtained_isr,
+        'Pred vocal sir': Av_sir,
+        'Pred bass sir': Ab_sir,
+        'Pred drums sir': Ad_sir,
+        'Pred other sir': Ao_sir,
+        'Pred Overall sir': Aobtained_sir,
+        'Pred vocal sar': Av_sar,
+        'Pred bass sar': Ab_sar,
+        'Pred drums sar': Ad_sar,
+        'Pred other sar': Ao_sar,
+        'Pred Overall sar': Aobtained_sar})
+
+    sdr.to_csv(path+'sdr_ge.csv')
+
+
+from tensorflow.keras.models import load_model
+from iwaveunnet_ge import GraphCNN
+
+# Load the model with custom objects
+custom_objects = {"GraphCNN": GraphCNN}
+
+path = "/home/anchal/Desktop/rajesh/UNet/iWaveUNet/ChebGCN/"
+#model = tf.keras.models.load_model(path+'iwaveunet_ge.h5', compile=False)
+model = load_model(path + 'iwaveunet_ge.h5', custom_objects=custom_objects, compile=False)
+
+calc_sdr(model, path)
